43d741e596b0abc4814b7d94129cd0135b5e925a
Darin <darnik22@gmail.com>
2021-06-15 06:40:46 +0000

    Fixed MB/s measuring

--- upload-files-flat.py
+++ upload-files-flat.py
@@ -11,15 +11,21 @@ odfs = OnedataFS(provider, token, force_direct_io=True, cli_args="--communicator
 space = odfs.opendir(space_name)
 st = ct = pt = time()
 file_count = 0
+if 'block_size' not in globals():
+    block_size = 1048576
+if 'block_count' not in globals():
+    block_count = 100
 space.makedir('dir-flat')
+print('Uploading', files_limit, 'files of size', 
+      block_size*block_count/1048576, 'MB...', flush=True)
 for i in range(files_limit):
-    f = subprocess.run(['./gbs', '1048576', '1', '1', 'w'], stdout=subprocess.PIPE)
+    f = subprocess.run(['./gbs', str(block_size), str(i), str(block_count), 'w'], stdout=subprocess.PIPE)
     space.writebytes('dir-flat'+'/file-'+str(i), f.stdout)
     file_count += 1
     ct = time()
     if ct-pt > 1:
         pt=ct
-        print('\r'+str(file_count)+" files in "+str(round(ct-st,2))+" seconds, avg rate: "+str(round(file_count*100/(ct-st),2))+" MB/s     ", end='', flush=True)
+        print('\r'+str(file_count)+" files in "+str(round(ct-st,2))+" seconds, avg rate: "+str(round(file_count*block_count*block_size/1048576/(ct-st),2))+" MB/s     ", end='', flush=True)
 ct = time()
-print('\r'+str(file_count)+" files in "+str(round(ct-st,2))+" seconds, avg rate: "+str(round(file_count*100/(ct-st),2))+" MB/s     ", flush=True)
+print('\r'+str(file_count)+" files in "+str(round(ct-st,2))+" seconds, avg rate: "+str(round(file_count*block_count*block_size/1048576/(ct-st),2))+" MB/s     ", flush=True)
 
